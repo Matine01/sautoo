606cc3ad9d01ed22efe0f1c5c7c790b450b67a03
Matine <61119446+Matine01@users.noreply.github.com>
2024-02-20 16:05:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -55,4 +55,4 @@ function leaveVC() {
 const keepAlive = require('./server.js');
 keepAlive();
 
-client.login("ODYwNjY3OTUzMTcwMjE5MDI4.GYe-4Q.0QT09PI67TX1Dda7hhoQPpERkrJ8tg4kRqSEOo");
+client.login(process.env.TOKEN);
